cbda30d1bb2325ffa9555329afa31bd004aa5cb3
nicko <nickogarces@gmail.com>
2024-04-18 20:28:20 -0400

    schema initial entities

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,23 +1,76 @@
 import {
-    pgTable,
-    serial,
-    text,
-    timestamp,
-    uniqueIndex,
-  } from 'drizzle-orm/pg-core';
+  pgTable,
+  serial,
+  text,
+  uniqueIndex,
+  integer,
+} from "drizzle-orm/pg-core";
 
-  export const ExampleTable = pgTable(
-    'users',
-    {
-      id: serial('id').primaryKey(),
-      name: text('name').notNull(),
-      email: text('email').notNull(),
-      image: text('image').notNull(),
-      createdAt: timestamp('createdAt').defaultNow().notNull(),
-    },
-    (users) => {
-      return {
-        uniqueIdx: uniqueIndex('unique_idx').on(users.email),
-      };
-    },
-  );
+export const Products = pgTable("products", {
+  id: serial("id").primaryKey(),
+  name: text("name").notNull(),
+  code: text("code").notNull(),
+  price: integer("price").notNull(),
+  categoryId: integer("category_id").references(() => Categories.id),
+  brandId: integer("brand_id").references(() => Brands.id),
+  supplierId: integer("supplier_id").references(() => Suppliers.id),
+});
+
+export const ProductWarehouse = pgTable("product_warehouse", {
+  productId: integer("product_id").references(() => Products.id),
+  warehouseId: integer("warehouse_id").references(() => Warehouses.id),
+  quantity: integer("quantity").notNull(),
+});
+
+export const Categories = pgTable(
+  "categories",
+  {
+    id: serial("id").primaryKey(),
+    name: text("name").notNull(),
+  },
+  (category) => {
+    return {
+      uniqueIdx: uniqueIndex("unique_idx").on(category.name),
+    };
+  }
+);
+
+export const Brands = pgTable(
+  "brands",
+  {
+    id: serial("id").primaryKey(),
+    name: text("name").notNull(),
+  },
+  (brand) => {
+    return {
+      uniqueIdx: uniqueIndex("unique_idx").on(brand.name),
+    };
+  }
+);
+
+export const Suppliers = pgTable(
+  "suppliers",
+  {
+    id: serial("id").primaryKey(),
+    name: text("name").notNull(),
+  },
+  (supplier) => {
+    return {
+      uniqueIdx: uniqueIndex("unique_idx").on(supplier.name),
+    };
+  }
+);
+
+export const Warehouses = pgTable(
+  "warehouses",
+  {
+    id: serial("id").primaryKey(),
+    name: text("name").notNull(),
+    address: text("address").notNull(),
+  },
+  (warehouse) => {
+    return {
+      uniqueIdx: uniqueIndex("unique_idx").on(warehouse.name),
+    };
+  }
+);
